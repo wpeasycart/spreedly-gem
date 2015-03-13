6a83144c7a3ff8744db0e25592541f1744b7a1c6
Joey Beninghove <joeybeninghove@gmail.com>
2015-03-13 17:20:24 -0400

    Add xml_doc accessor on base Model to expose access to the raw XML, because sometimes that's handy to see :)

--- lib/spreedly/model.rb
+++ lib/spreedly/model.rb
@@ -8,7 +8,10 @@ module Spreedly
     field :token
     field :created_at, :updated_at, type: :date_time
 
+    attr_reader :xml_doc
+
     def initialize(xml_doc)
+      @xml_doc = xml_doc
       initialize_fields(xml_doc)
     end
 
